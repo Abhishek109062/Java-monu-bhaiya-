3ced61773ca5b3fc4eee05657b8840e27b4dabb2
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-19 11:53:30 -0700

    dd

--- 17.08/fibonacci.java
+++ 17.08/fibonacci.java
@@ -1,15 +1,9 @@
 import java.util.*;
 class fibonacci{
+  
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int a=0;
-        int b=1;
-        for(int x=1;x<=n;x++){
-            int c=a+b;
-            a=b;
-            b=c;
-        }
-        System.out.println(a);
+    
+        for(byte x=0;x<128;x++)
+        System.out.println(x);
     }
 }
---
+++ 18.08/binary.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class binary {
+    public static void main(String[] args) {
+         int x=-80;
+        System.out.println(x/5);    
+    }
+    
+}
