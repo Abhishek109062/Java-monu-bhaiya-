71053d34007e6333d1cdd4f0eb8b6fc47e1f28b1
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-24 14:23:53 +0530

    commit

--- 18.08/binary.java
+++ 18.08/binary.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class binary {
-    public static void main(String[] args) {
-         int x=-80;
-        System.out.println(x/5);    
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+		char ch[]=sc.next().toCharArray();
+		for(int x=ch.length-1;x>=0;x--){
+			if((char)('9'- ch[x]+48)<ch[x])
+				ch[x]=(char)('9'- ch[x]+48);
+		}
+		
+	System.out.println(1&2);
     }
-    
 }
