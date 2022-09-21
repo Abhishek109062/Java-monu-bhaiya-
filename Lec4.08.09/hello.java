44d4f3bd114f478ec59509daa0f8cad0658976ec
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-21 13:26:52 +0530

    done

---
+++ Lec4.08.09/hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        String s="Hello";
+        for(int x=1;x<=5;x++){
+            for(int y=1;y<=5-x+1;y++){
+                System.out.println(s.substring((y-1),(y-1+x)));
+            }
+            //System.out.println();
+        }
+    }    
+}
